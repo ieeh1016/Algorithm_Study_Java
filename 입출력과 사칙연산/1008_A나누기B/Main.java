64f01f0ba6cc36acabb6b00d849f025fcaee403a
영우 <ieeh0122@naver.com>
2022-01-07 13:30:15 +0900

    baekjoon: 1008
    백준 1008: A/B

---
+++ 입출력과 사칙연산/1008_A나누기B/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double a= scanner.nextDouble();
+		double b= scanner.nextDouble();
+		
+		double c=a/b;
+		System.out.print(c);
+		
+		scanner.close();
+	}
+}
