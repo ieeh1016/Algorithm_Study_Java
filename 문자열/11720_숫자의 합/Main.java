7d59d98f885fef9ad4e1723914c7011480c5d044
영우 <ieeh0122@naver.com>
2022-01-10 22:15:33 +0900

    baekjoon: 11720
    백준 11720: 숫자의 합

---
+++ 문자열/11720_숫자의 합/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class Main {
+ 
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		String str = scanner.next();
+		
+		int sum=0;
+		
+		for(int i=0; i<a; i++) {
+			sum=sum+str.charAt(i)-'0';
+		}
+		System.out.print(sum);
+	}
+
+ 
+}
+
+
+	
