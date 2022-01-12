76710666dd63cbc19110f481a8c7041ec99d9d2e
영우 <ieeh0122@naver.com>
2022-01-12 12:31:20 +0900

    baekjoon: 10757
    백준 10757: 큰 수 A+B

---
+++ 기본 수학 1단계/10757_큰 수 A+B/Main.java
@@ -0,0 +1,21 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+ 
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		BigInteger a =scanner.nextBigInteger();
+		BigInteger b =scanner.nextBigInteger();
+		
+		
+		BigInteger result=a.add(b);
+		
+		System.out.print(result);
+				
+	}
+}
+
+
+	
