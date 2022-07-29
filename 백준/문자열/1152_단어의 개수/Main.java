c2782d26f577afb656a8621ed803eca57beb361c
현영우 <ieeh0122@naver.com>
2022-07-29 21:37:44 +0900

    1152
    단어의 개수

--- 백준/문자열/1152_단어의 개수/Main.java
+++ 백준/문자열/1152_단어의 개수/Main.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
+
 public class Main {
- 
+
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
 		Scanner scanner = new Scanner(System.in);
+		
+		//StringTokenizer은 문자열을 우리가 지정한 구분자로 문자열을 쪼개주는 클래스이다.
 		
 		String str = scanner.nextLine();
 		StringTokenizer st = new StringTokenizer(str," ");
 		
-
-		System.out.println(st.countTokens());	
+		System.out.println(st.countTokens());
 		
-		
+		scanner.close();
 	}
+
 }
-
-
-	
