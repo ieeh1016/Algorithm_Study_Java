6e6c7172f3b9bbb9ff3ec36f42ab076429f219af
YW <ieeh0122@naver.com>
2022-09-11 11:59:51 +0900

    최대 매출

---
+++ 인프런자바코딩테스트대비/섹션 3.Tow pointers, Sliding window/3. 최대 매출/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class Main {
+	public int solution(int n, int k, int[] arr){
+		
+		int answer = 0;
+		
+		int[] array = new int[n];
+		int sum = 0;
+		for(int i=0; i<k; i++){
+			sum = sum + arr[i];
+		}
+		
+		for(int i=k; i<n; i++) {
+			sum = sum + arr[i] - arr[i-k];
+			if(sum > answer) {
+				answer = sum;
+			}
+		}
+		
+		return answer;
+	}
+	
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		int n=kb.nextInt();
+		int k=kb.nextInt();
+		int[] arr=new int[n];
+		for(int i=0; i<n; i++){
+			arr[i]=kb.nextInt();
+		}
+		System.out.print(T.solution(n, k, arr));
+	}
+}
