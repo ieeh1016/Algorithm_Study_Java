ca425fb62c26a27eac946c294df40a4c8d09e665
YW <ieeh0122@naver.com>
2022-09-11 18:44:02 +0900

    괄호문자제거

---
+++ 인프런자바코딩테스트대비/섹션 5.Stack, Queue/2. 괄호문자제거/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+class Main {
+	public String solution(String str) {
+		String answer = "";
+
+		Stack<Character> stack = new Stack<>();
+		for (char x : str.toCharArray()) {
+			if (x == '(') {
+				stack.push(x);
+			} else if (x == ')') {
+				stack.pop();
+			} else {
+				if (stack.isEmpty()) {
+					answer = answer + x;
+				}
+			}
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		String str = kb.next();
+		System.out.println(T.solution(str));
+	}
+}
