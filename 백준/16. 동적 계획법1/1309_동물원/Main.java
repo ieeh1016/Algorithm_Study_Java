46431999ca5d29a643801cf611f155ece8c4a208
Young Woo, Hyun <ieeh0122@naver.com>
2022-11-17 13:56:44 +0900

    동물원

---
+++ 백준/16. 동적 계획법1/1309_동물원/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);		
+		int n = sc.nextInt();
+		int[] dp = new int[n+2];
+		
+		dp[1] = 3;
+		dp[2] = 7;
+		
+		for(int i=3; i<=n; i++) {
+			dp[i] = (2*dp[i-1] + dp[i-2])%9901;
+		}
+		System.out.print(dp[n]);
+		sc.close();
+	}
+
+}
+
